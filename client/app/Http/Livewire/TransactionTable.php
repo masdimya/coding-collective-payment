0fa4450b9e7304d92cf3df43cadd045b8e306ec6
dimas.aulia <dimas.aulia@elnusa.co.id>
2024-05-27 05:25:35 +0700

    separate view into livewire components

---
+++ client/app/Http/Livewire/TransactionTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TransactionTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.transaction-table');
+    }
+}
---
+++ client/resources/views/livewire/balance.blade.php
@@ -0,0 +1,29 @@
+<div>
+    <div class="text-xl font-semibold text-gray-800 mb-5">
+      Balance
+    </div>
+    <div class="flex">
+      <div class="w-1/5 flex flex-col bg-white border border-t-4 border-t-green-600 shadow-sm rounded-xl mr-10">
+        <div class="p-4 md:p-5">
+          <h3 class="text-lg font-bold text-gray-800 ">
+            Local Balance
+          </h3>
+          <p class="mt-2 font-bold text-gray-900 text-4xl">
+            10000.00
+          </p>
+  
+        </div>
+      </div>
+      <div class="w-1/5 flex flex-col bg-white border border-t-4 border-t-blue-600 shadow-sm rounded-xl ">
+        <div class="p-4 md:p-5">
+          <h3 class="text-lg font-bold text-gray-800 ">
+            Online Balance
+          </h3>
+          <p class="mt-2 font-bold text-gray-900 text-4xl">
+            10000.00
+          </p>
+  
+        </div>
+      </div>
+    </div>
+  </div>
