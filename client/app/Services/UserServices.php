ede0c3c256f530458267a51d8692710ba202a6fe
dimas.aulia <dimas.aulia@elnusa.co.id>
2024-05-27 12:56:21 +0700

    add withdraw and debit  function in userService

--- client/app/Services/UserServices.php
+++ client/app/Services/UserServices.php
@@ -14,5 +14,53 @@ class UserServices
   public function getTransactions(){
     return UserTransaction::all();
   }
+
+  protected function generateOrderId($type){
+    $total = UserTransaction::count();
+    return $type . $total + 1;
+  }
+
+  public function generateWithdrawOrderId(){
+    return $this->generateOrderId('withdraw');
+  }
+
+  public function generateDepositOrderId(){
+    return $this->generateOrderId('deposit');
+  }
+
+  public function updateWithdrawBalance(){
+
+  }
+
+  public function updateDepostiBalance(){
+
+  }
+
+  protected function createTransaction(){
+
+  }
+
+  protected function updateTransaction(){
+
+  }
+
+  public function createWithdrawProcess(){
+    $this->createTransaction();
+  }
+
+  public function createDepositProcess(){
+    $this->createTransaction();
+  }
+
+  public function updateWithdrawProcess(){
+    $this->updateTransaction();
+  }
+
+  public function updateDepositProcess(){
+    $this->updateTransaction();
+  }
+
+
+
 }
 
